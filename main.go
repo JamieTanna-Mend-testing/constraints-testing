a6e8b90f608e8ac05aa9b602616320b9183ea72a
Jamie Tanna <jamie.tanna@mend.io>
2026-04-28 13:19:40 +0100

    another

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/onsi/ginkgo"
 )
 
 func main() {
 	u := uuid.MustParse(uuid.NewString())
 	fmt.Printf("u: %v\n", u)
+
+	fmt.Println(ginkgo.Context("", func() {}))
 }
